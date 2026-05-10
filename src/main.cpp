de4768d32774bb10b5b0cc831767f10a4e46b80d
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-05-10 21:31:10 +0200

    Created main.cpp file init

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <Windows.h>
+#include "WindowManager.hpp"
 
 int main(int argc, char **argv) {
-    std::cout << "hello world!" << std::endl;
+    
+    // Start Hook window hook
+    WindowManager::getInstance().MOUSE_start();
+
+    MSG _msg;
+    while (GetMessage(&_msg, nullptr, 0, 0)) {
+        TranslateMessage(&_msg);
+        DispatchMessage(&_msg);
+    }
+
+    WindowManager::getInstance().MOUSE_stop();
     return 0;
 }
