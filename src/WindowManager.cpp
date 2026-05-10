1ed3bb5aa83f1387ce780f44a487914bcebae868
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-05-10 22:57:51 +0200

    Rewrote logic for window position and mouse movement

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -7,28 +7,56 @@ WindowManager &WindowManager::getInstance() {
 }
 
 // Intercept Windows mouse events
-void WindowManager::MOUSE_start() { m_mouse_hook = SetWindowsHookEx(WH_MOUSE_LL, MOUSE_process, GetModuleHandle(NULL), 0); }
-void WindowManager::MOUSE_stop()
-{
+void WindowManager::MOUSE_start() { 
+    m_mouse_hook = SetWindowsHookEx(WH_MOUSE_LL, MouseProcess, GetModuleHandle(NULL), 0);
+    m_keyboard_hook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProcess, GetModuleHandle(NULL), 0);
+}
+
+void WindowManager::MOUSE_stop() {
     if (m_mouse_hook) {
 
         UnhookWindowsHookEx(m_mouse_hook);
         m_mouse_hook = nullptr;
+
+    } else if (m_keyboard_hook) {
+        UnhookWindowsHookEx(m_keyboard_hook);
+        m_keyboard_hook = nullptr;        
     }
+    
 }
 
 // Clean-up incase of crash or close
-WindowManager::~WindowManager() {
-    MOUSE_stop();
+WindowManager::~WindowManager() { MOUSE_stop(); }
+
+// Main keyboard event callback
+LRESULT CALLBACK WindowManager::KeyboardProcess(int n_code, WPARAM w_param, LPARAM l_Param) {
+    if (n_code >= 0) {
+        auto* keyboard_struct = reinterpret_cast<KBDLLHOOKSTRUCT*> (l_Param);
+        auto& _manager = getInstance();
+
+        if (keyboard_struct -> vkCode == VK_LWIN || keyboard_struct -> vkCode == VK_RWIN) {
+
+            if (w_param == WM_KEYUP || w_param == WM_SYSKEYUP) {
+                if (_manager.m_isDragging) {
+                    _manager.Handle_Mouse_UP();
+                }
+            }
+        }
+    }
+
+    return CallNextHookEx(nullptr, n_code, w_param, l_Param);
 }
 
-
-// Main event callback
-LRESULT CALLBACK WindowManager::MOUSE_process(int n_code, WPARAM w_param, LPARAM l_Param) {
+// Main mouse event callback
+LRESULT CALLBACK WindowManager::MouseProcess(int n_code, WPARAM w_param, LPARAM l_Param) {
     if (n_code >= 0) {
         
         auto* mouse_struct = reinterpret_cast<MSLLHOOKSTRUCT*> (l_Param);
-        auto& _manager     = getInstance();
+        auto& _manager = getInstance();
+
+        if (mouse_struct -> flags & LLKHF_INJECTED) {
+            return CallNextHookEx(nullptr, n_code, w_param, l_Param);
+        }
 
         // 0x8000 => Bitmask check
         bool super_press = (GetAsyncKeyState(VK_LWIN) & 0x8000) || (GetAsyncKeyState(VK_RWIN) & 0x8000);
@@ -40,7 +68,7 @@ LRESULT CALLBACK WindowManager::MOUSE_process(int n_code, WPARAM w_param, LPARAM
 
         else if (w_param == WM_MOUSEMOVE && _manager.m_isDragging) {
             _manager.Handle_Mouse_MOVE(mouse_struct);
-            return 1;
+            //return 1;
         }
 
         else if (w_param == WM_LBUTTONUP) {
@@ -57,30 +85,31 @@ void WindowManager::Hanlde_Mouse_DOWN(MSLLHOOKSTRUCT* mouse_struct) {
 
     if (m_target_window) {
 
-        // Force it to grab parent window instead
         m_target_window = GetAncestor(m_target_window, GA_ROOT);
-        m_lastMouse_pos = mouse_struct -> pt;
-        m_isDragging    = true;
+
+        RECT _rect;
+        if (GetWindowRect(m_target_window, &_rect)) {
+
+            m_offsetX    = mouse_struct -> pt.x - _rect.left;
+            m_offsetY    = mouse_struct -> pt.y - _rect.top;
+            m_isDragging = true;
+        }
     }
 }
 
 void WindowManager::Handle_Mouse_MOVE(MSLLHOOKSTRUCT* mouse_struct) {
-    RECT _rect;
 
-    // Get position and size of window
-    if (GetWindowRect(m_target_window, &_rect)) {
-
-        int _dX = mouse_struct -> pt.x - m_lastMouse_pos.x; 
-        int _dY = mouse_struct -> pt.y - m_lastMouse_pos.y;
+    if (m_target_window) {
+        int _newY = mouse_struct -> pt.x - m_offsetX;
+        int _newX = mouse_struct -> pt.y - m_offsetY;
         
         SetWindowPos(
-            m_target_window, nullptr,
-            _rect.left + _dX,
-            _rect.top + _dY,
-            0, 0,
+            m_target_window, nullptr, 
+            _newX, _newY, 0, 0,
             SWP_NOSIZE | SWP_NOZORDER
         );
     }
+
 }
 
 
