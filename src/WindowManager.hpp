69f9c2e7aa199723ea1d50d94cd541c73c53d690
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-05-10 22:53:44 +0200

    Updated method signatures

--- src/WindowManager.hpp
+++ src/WindowManager.hpp
@@ -9,20 +9,24 @@ class WindowManager {
 
         void MOUSE_start();
         void MOUSE_stop();
+        void Handle_Mouse_UP();
 
     private:
         WindowManager() = default;
         ~WindowManager();
 
-        static LRESULT CALLBACK MOUSE_process(int n_Code, WPARAM w_Param, LPARAM l_Param);
+        static LRESULT CALLBACK MouseProcess(int n_Code, WPARAM w_Param, LPARAM l_Param);
+        static LRESULT CALLBACK KeyboardProcess(int n_code, WPARAM w_param, LPARAM l_Param);
 
         void Handle_Mouse_MOVE(MSLLHOOKSTRUCT* mouse_struct);
         void Hanlde_Mouse_DOWN(MSLLHOOKSTRUCT* mouse_struct);
-        void Handle_Mouse_UP();
 
         HHOOK m_mouse_hook     = nullptr;
+        HHOOK m_keyboard_hook   = nullptr;
         HWND  m_target_window  = nullptr;
-        POINT m_lastMouse_pos  = {0, 0};
+        
+        int m_offsetX = 0;
+        int m_offsetY = 0;
 
         bool m_isDragging      = false;
 };
