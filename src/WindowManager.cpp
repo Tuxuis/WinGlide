f62746d01f7f2d092b746cc10330753eccd703d8
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-05-11 21:32:55 +0200

    Updated to include new DragController class

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -37,8 +37,8 @@ LRESULT CALLBACK WindowManager::KeyboardProcess(int n_code, WPARAM w_param, LPAR
         if (keyboard_struct -> vkCode == VK_LWIN || keyboard_struct -> vkCode == VK_RWIN) {
 
             if (w_param == WM_KEYUP || w_param == WM_SYSKEYUP) {
-                if (_manager.m_isDragging) {
-                    _manager.Handle_Mouse_UP();
+                if (_manager.m_drag._is_Dragging()) {
+                    _manager.m_drag.Drag_end();
                 }
             }
         }
@@ -62,17 +62,17 @@ LRESULT CALLBACK WindowManager::MouseProcess(int n_code, WPARAM w_param, LPARAM 
         bool super_press = (GetAsyncKeyState(VK_LWIN) & 0x8000) || (GetAsyncKeyState(VK_RWIN) & 0x8000);
 
         if (w_param == WM_LBUTTONDOWN && super_press) {
-            _manager.Hanlde_Mouse_DOWN(mouse_struct);
+            _manager.m_drag.Drag_begin(mouse_struct);
             return 1;
         }
 
-        else if (w_param == WM_MOUSEMOVE && _manager.m_isDragging) {
-            _manager.Handle_Mouse_MOVE(mouse_struct);
+        else if (w_param == WM_MOUSEMOVE && _manager.m_drag._is_Dragging()) {
+            _manager.m_drag.Drag_update(mouse_struct);
             //return 1;
         }
 
         else if (w_param == WM_LBUTTONUP) {
-            _manager.Handle_Mouse_UP();
+            _manager.m_drag.Drag_end();
         }
     }
 
@@ -81,53 +81,14 @@ LRESULT CALLBACK WindowManager::MouseProcess(int n_code, WPARAM w_param, LPARAM 
 
 
 void WindowManager::Hanlde_Mouse_DOWN(MSLLHOOKSTRUCT* mouse_struct) {
-    m_target_window = WindowFromPoint(mouse_struct -> pt);
-
-    if (m_target_window) {
-
-        m_target_window = GetAncestor(m_target_window, GA_ROOT);
-
-        RECT _rect;
-        if (GetWindowRect(m_target_window, &_rect)) {
-
-            m_offsetX    = mouse_struct -> pt.x - _rect.left;
-            m_offsetY    = mouse_struct -> pt.y - _rect.top;
-            m_isDragging = true;
-        }
-    }
+    m_drag.Drag_begin(mouse_struct);
 }
 
 void WindowManager::Handle_Mouse_MOVE(MSLLHOOKSTRUCT* mouse_struct) {
-
-    if (m_target_window) {
-        int _newX = mouse_struct -> pt.x - m_offsetX;
-        int _newY = mouse_struct -> pt.y - m_offsetY;
-        
-        SetWindowPos(
-            m_target_window, nullptr, 
-            _newX, _newY, 0, 0,
-            SWP_NOSIZE | SWP_NOZORDER
-        );
-    }
-
+    m_drag.Drag_update(mouse_struct);
 }
 
 
 void WindowManager::Handle_Mouse_UP() {
-    
-    if (m_isDragging) {
-        INPUT _input[2] = {};
-        
-        // 0xE8 => Unassigned
-        _input[1].ki.dwFlags  = KEYEVENTF_KEYUP;
-        _input[0].type        = INPUT_KEYBOARD;
-        _input[1].type        = INPUT_KEYBOARD;
-        _input[0].ki.wVk      = 0xE8;
-        _input[1].ki.wVk      = 0xE8;
-
-        SendInput(2, _input, sizeof(INPUT));
-        m_isDragging = false;
-    }
-
-    m_target_window = nullptr;
+    m_drag.Drag_end();
 }
