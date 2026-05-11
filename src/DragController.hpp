6503fc64dc68bd84d0d015a0575103843dd84a04
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-05-11 21:32:37 +0200

    Created DragController header file

---
+++ src/DragController.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Windows.h>
+
+class DragController {
+
+    public:
+        void Drag_begin(const MSLLHOOKSTRUCT* mouse_struct);
+        void Drag_update(const MSLLHOOKSTRUCT* mouse_struct);
+        void Drag_end();
+
+        // Read only for drag state
+        bool _is_Dragging() const { return m_isDragging; }
+    
+    private:
+        HWND m_target_window = nullptr;
+
+        int m_offsetX = 0;
+        int m_offsetY = 0;
+
+        bool m_isDragging = false;
+};
