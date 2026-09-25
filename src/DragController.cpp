8cf1238fa745ba643353dc4bbf4c5cb3e200fde2
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-09-25 19:46:47 +0200

    Fixed inconsistent issues with `Drag_Begin`, now nothing gets stored unless the drag starts

--- src/DragController.cpp
+++ src/DragController.cpp
@@ -3,58 +3,39 @@
 
 
 
-void DragController::Drag_begin(const MSLLHOOKSTRUCT* mouse_struct) {
+bool DragController::Drag_begin(const MSLLHOOKSTRUCT* mouse_struct) {
+
+    HWND target = Win32Api::Window_findTarget(mouse_struct -> pt);
     
-    m_target_window = Win32Api::Window_FromPoint(mouse_struct -> pt);
-    m_sendWinKeyTap = Win32Api::Digital_isWinKeyDown();
-    
-    if (!m_target_window) {
-        return;
+    RECT rect;
+
+    if (!target || !Win32Api::Window_getRect(target, rect)) {
+        return false;
     }
 
-    m_target_window = Win32Api::Window_getRoot(m_target_window);
+    m_target_window = target;
+    m_offsetX       = mouse_struct -> pt.x - rect.left;
+    m_offsetY       = mouse_struct -> pt.y - rect.top;
+    m_isActive      = true;
 
-    RECT _rect;
-    if (Win32Api::Window_getRect(m_target_window, _rect)) {
-
-        // Put offset on cursor so the window wont jump
-        m_offsetX = mouse_struct -> pt.x - _rect.left;
-        m_offsetY = mouse_struct -> pt.y - _rect.top;
-
-        m_isActive = true;
-    }
+    return true;
 }
 
 
 void DragController::Drag_update(const MSLLHOOKSTRUCT* mouse_struct) {
 
-    if (!m_target_window) {
+    if (!m_isActive) {
         return;
     }
     
-    int _newX = mouse_struct -> pt.x - m_offsetX;
-    int _newY = mouse_struct -> pt.y - m_offsetY;
+    const int newX = mouse_struct -> pt.x - m_offsetX;
+    const int newY = mouse_struct -> pt.y - m_offsetY;
 
-    Win32Api::Window_moveNoSize(m_target_window, _newX, _newY);
+    Win32Api::Window_moveNoSize(m_target_window, newX, newY);
 }
 
 
 void DragController::Drag_end() {
-
-    if (m_isActive) {
-        
-        // Using 0xE8 because it is an "virtual" key on Windows. This
-        // helps by preventing SUPER key to open the Windows menu while
-        // the user is dragging.
-
-        // Only send key tap if we started with the Win (SUPER) key.
-        if (m_sendWinKeyTap) {
-            Win32Api::Signal_VirtualKeyTap(0xE8);
-        }
-
-        m_isActive = false;
-    }
-    
-    m_sendWinKeyTap = false;
+    m_isActive      = false;
     m_target_window = nullptr;
 }
