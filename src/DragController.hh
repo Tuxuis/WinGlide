29431e58ba9556c00e7c8b989afe346ed7106b84
Tux <84027033+Tuxuis@users.noreply.github.com>
2026-09-25 22:00:56 +0200

    Intergrated WindowsMover into DragController

--- src/DragController.hh
+++ src/DragController.hh
@@ -2,17 +2,22 @@
 
 #include <Windows.h>
 
+class WindowMover;
+
 class DragController {
 
     public:
+        explicit DragController(WindowMover& mover) : m_mover(mover) {}
+
         bool Drag_begin(const MSLLHOOKSTRUCT* mouse_struct);
         void Drag_update(const MSLLHOOKSTRUCT* mouse_struct);
         void Drag_end();
 
-        // Read only for drag state
         bool isActive() const { return m_isActive; }
     
     private:
+        WindowMover& m_mover;
+
         HWND m_target_window = nullptr;
 
         int m_offsetX = 0;
